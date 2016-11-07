b004378a84daf181456223214ecba810f8a48b80
jandechent <valk@Marie-iMac.fritz.box>
2016-11-07 17:51:33 +0100

    __main__==main

--- fritzbox-logger/__main__.py
+++ fritzbox-logger/__main__.py
@@ -4,13 +4,16 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 import logging
-logging.basicConfig(level=logging.WARN)
 
 from Plotter import Plotter
 from Logger import Logger
 
-f=Logger("fritz.box", "smarthome","smarthome",delayInSeconds=15*60)
-f.start();
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARN)
+    logging.getLogger("Logger").setLevel(logging.INFO)
 
-while f.isAlive():
-    p=Plotter(plotnow=True,timeWindowInH=8)
+    f=Logger("fritz.box", "smarthome","smarthome",delayInSeconds=15*60)
+    f.start();
+
+    while f.isAlive():
+        p=Plotter(plotnow=True,timeWindowInH=8)
